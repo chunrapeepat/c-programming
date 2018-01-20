09a60d687d51692e89312e2396c6dffa2de8ae42
chunza2542 <chunza2542@hotmail.com>
2018-01-20 17:32:37 +0700

    pointer on struct

--- immediate/struct.c
+++ immediate/struct.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-struct student
-{
-  char name[15];
-  int rollno;
+struct student {
+  char *name;
   int age;
 };
 
+void mutateStudent(struct student *);
+
 int main()
 {
-  struct student chun = {"rapeepat", 1234, 18};
-  printf("[%d] Hello! my name is %s age %d\n", chun.rollno, chun.name, chun.age);
+  struct student chun = {"eieiza", 50};
 
+  printf("%s\n", chun.name);
+  mutateStudent(&chun);
+  printf("%s\n", chun.name);
   return 0;
+}
+
+void mutateStudent(struct student *person) {
+  person->name = "chunza2542";
 }
